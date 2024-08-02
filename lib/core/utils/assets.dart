5507e0e48ccc231f6a71fdebea1d1db8b3335f0f
meriem <meriembelaid44@gmail.com>
2024-08-02 17:53:16 +0100

    App colors

---
+++ lib/core/utils/assets.dart
@@ -0,0 +1,16 @@
+class Assets {
+  Assets._();
+  
+  /// Assets for imagesImage1
+  /// assets/images/image1.jpg
+  static const String imagesImage1 = "assets/images/image1.jpg";
+
+  /// Assets for imagesImage2
+  /// assets/images/image2.jpg
+  static const String imagesImage2 = "assets/images/image2.jpg";
+
+  /// Assets for imagesImage3
+  /// assets/images/image3.jpeg
+  static const String imagesImage3 = "assets/images/image3.jpeg";
+}
+
